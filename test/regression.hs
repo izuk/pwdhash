499ca5bff4ab74240b9fc14b0981c24985ba6d69
Itai Zukerman <zukerman@math-hat.com>
2021-11-03 21:09:26 -0700

    Changed regression test to only try printable ASCII characters.

--- test/regression.hs
+++ test/regression.hs
@@ -1,24 +1,35 @@
 import Codec.Pwdhash (pwdhash)
+import Data.Char (isAscii, isPrint)
 import System.Process (readProcess)
 import Test.Framework (Test, defaultMain)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
-import Test.QuickCheck ((==>), Property)
+import Test.QuickCheck (Property)
+import Test.QuickCheck.Arbitrary (Arbitrary(..))
+import Test.QuickCheck.Gen (listOf, suchThat)
 import Test.QuickCheck.Monadic (assert, monadicIO, run)
 
+
+newtype S = S String
+  deriving (Eq, Ord, Show, Read)
+
+okS :: Char -> Bool
+okS s = isAscii s && isPrint s
+
+instance Arbitrary S where
+  arbitrary = S <$> (listOf $ arbitrary `suchThat` okS)
+  shrink (S s) = S <$> shrink s
 
 execNode :: String -> String -> IO String
 execNode password realm = readProcess "node" ["js/pwdhash.js", password, realm] ""
 
-noNull :: String -> Bool
-noNull = all (/= '\NUL')
-
-prop_regression :: String -> String -> Property
-prop_regression password realm = noNull (password ++ realm) ==> prop
-  where
-    prop = monadicIO $ do
-      expected <- run $ fmap init $ execNode password realm
-      let actual = pwdhash password realm
-      assert $ actual == expected
+prop_regression :: S -> S -> Property
+prop_regression password' realm' = monadicIO $ do
+  let S password = password'
+      S realm = realm'
+  run $ print (password, realm)
+  expected <- run $ fmap init $ execNode password realm
+  let actual = pwdhash password realm
+  assert $ actual == expected
 
 tests :: [Test]
 tests =
